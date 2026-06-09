7eca504ff6c44a6cdefa2fa7dd93a19e6124fc04
xxharry19xx <123268809+xxharry19xx@users.noreply.github.com>
2026-06-08 22:50:26 -0500

    CORRECCION DE DATOS

--- tienda-utiles/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-const SUPABASE_URL = 'https://oscituaklxfonvcmsrsc.supabase.co'
-const SUPABASE_ANON = 'sb_publishable_jsi8l3TgCePI9b0ZY1x7rg_s_nitFD0'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
